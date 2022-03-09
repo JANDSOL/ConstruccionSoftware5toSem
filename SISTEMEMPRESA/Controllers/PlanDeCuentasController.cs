f9d5541beb77f8e5834b67f01f898ed1ee9c7e72
Juan Andrade <jpas.juan01@gmail.com>
2022-03-09 16:15:33 -0500

    Create index page to PLAN DE CUENTAS.

---
+++ SISTEMEMPRESA/Controllers/PlanDeCuentasController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SISTEMEMPRESA.Models;
+
+namespace SISTEMEMPRESA.Controllers
+{
+    public class PlanDeCuentasController : Controller
+    {
+        private ConnDbSISTEMAEMPRESA db = new ConnDbSISTEMAEMPRESA();
+        // GET: PlanDeCuentas
+        public ActionResult Index()
+        {
+            return View(db.CNTBPLANDECUENTA.ToList());
+        }
+    }
+}
